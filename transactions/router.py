0bf8ab4873e3c1cbe553ce20ec88a5a846e0c2ad
Sam <sammacciocca@gmail.com>
2026-04-18 17:14:41 +1000

    feature add withdrawl endpoint with transactions recording

--- transactions/router.py
+++ transactions/router.py
@@ -22,3 +22,26 @@ def deposit(account_id: int, amount: float, description: str):
         "description": description
     })
     return {"account_id": account_id, "new_balance": new_balance}
+
+@router.post("/withdrawl")
+def waithdrawl(account_id: int, amount: float,description: str):
+    account = fake_db["accounts"].get(account_id)
+    if not account:
+        return{"error": "Account not found"}
+
+    balance = account["balance"]
+    new_balance = balance - amount
+    if amount > balance:
+        return{"error": "Insufficient Funds"}
+    
+    balance = new_balance
+
+    fake_db["accounts"][account_id]["balance"] = new_balance
+    fake_db["transactions"].append({
+        "account_id": account_id,
+        "type": "waithdrawl",
+        "amount": amount,
+        "balance_after": new_balance,
+        "description": description
+    })
+    return {"account_id": account_id, "new_balance": new_balance}
