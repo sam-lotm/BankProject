b5455160651d45ebd0035ec7582e27cf085d1830
Sam <sammacciocca@gmail.com>
2026-04-23 13:48:44 +1000

    feat: add HTTPException error handling and duplicate validation

--- accounts/router.py
+++ accounts/router.py
@@ -2,18 +2,21 @@ from fastapi import APIRouter
 from database import fake_db
 from datetime import date
 from pydantic import EmailStr
+from fastapi import APIRouter, HTTPException
 router = APIRouter()
 
 @router.post("/create")
 def create_account(owner_name: str, DOB: date, Ph_no: str, email: EmailStr, balance: float = 0.0):
     age = (date.today() - DOB).days //365
-    if age< 18:
-       return {"error": "Must be 18 or older to open an account"}
+    if age < 18:
+        raise HTTPException(status_code=400, detail="Must be 18 or older to open an account")
+
+
     for account in fake_db["accounts"].values():
         if account["Phone_Number"] == Ph_no:
-            return {"error": "Number already registered"}
+            raise HTTPException(status_code=400, detail="Number already registered")
         if account["Email"] == email:
-            return {"error": "Email already registered"}
+            raise HTTPException(status_code=400, detail="Email already registered")
 
     account_id = len(fake_db["accounts"]) + 1
     fake_db["accounts"][account_id] = {
